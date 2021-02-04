69635a2e4ac2d0389ce3db9b777b13b2f384483f
TanithS <77208045+TanithS@users.noreply.github.com>
2021-02-04 11:46:17 -1000

    Completed Parts 1-4 of Hackwich #3

--- Hackwich3/ViewController.swift
+++ Hackwich3/ViewController.swift
@@ -20,24 +20,54 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-    @IBAction func changeColorButtonPressed(_ sender: Any) {
+    @IBAction func changeColorButtonPressed(_ sender: Any)
+    {
         
         //Creating a conditional statement that uses one of our variables and assigns it to a string that evaluates to false.
         
         if firstString == "The background color will turn green."
-        
         {
             //if block
             self.view.backgroundColor = UIColor.red
+            //Problem Set 1:
+            firstLabel.textColor = UIColor.red
         }
         
         else
-        
         {
             //else block
             self.view.backgroundColor = UIColor.blue
+            //Problem Set 1:
+            secondLabel.textColor = UIColor.blue
         }
     
     }
 
+    @IBOutlet weak var firstLabel: UILabel!
+    @IBOutlet weak var secondLabel: UILabel!
+    
+    //Problem Set 2:
+    
+    var stringNumeroUno = "I love coding!"
+    var stringNumeroDos = "I hate coding!"
+    
+    @IBAction func magicButtonPressed(_ sender: Any)
+    {
+        if stringNumeroUno == "I love coding!"
+        {
+            self.view.backgroundColor = UIColor.green
+            thirdLabel.textColor = UIColor.green
+            print("I completed both problem sets.")
+        }
+        
+        else
+        {
+            self.view.backgroundColor = UIColor.yellow
+            thirdLabel.textColor = UIColor.yellow
+            print("I did not complete both problem sets.")
+        }
+        
+    }
+    
+    @IBOutlet weak var thirdLabel: UILabel!
 }
