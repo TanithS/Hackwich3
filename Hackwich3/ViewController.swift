3104d9e7a61bd5be1b03bfe1f84b97173d55fc6a
TanithS <77208045+TanithS@users.noreply.github.com>
2021-02-04 10:01:36 -1000

    Completed Parts 1-3 of Hackwich #3

--- Hackwich3/ViewController.swift
+++ Hackwich3/ViewController.swift
@@ -8,12 +8,36 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+
+//Global variables are visible to the entire program. Local variables are created within functions, which means they are not accessible to the rest of the program.
+    
+    var firstString = "The background color will turn blue."
+    var secondString = "The background color will turn green."
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func changeColorButtonPressed(_ sender: Any) {
+        
+        //Creating a conditional statement that uses one of our variables and assigns it to a string that evaluates to false.
+        
+        if firstString == "The background color will turn green."
+        
+        {
+            //if block
+            self.view.backgroundColor = UIColor.red
+        }
+        
+        else
+        
+        {
+            //else block
+            self.view.backgroundColor = UIColor.blue
+        }
+    
+    }
 
 }
-
